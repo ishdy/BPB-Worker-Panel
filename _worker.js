43a7aa2bc8459f3c66f96972e72c12576a881a18
ishdy <41664425+ishdy@users.noreply.github.com>
2024-08-05 22:26:33 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -10,7 +10,7 @@ import { connect } from 'cloudflare:sockets';
 let userID = 'd430a4ca-1508-47b2-86fe-504a5ac2263b';
 
 // https://www.nslookup.io/domains/bpb.yousef.isegaro.com/dns-records/
-const proxyIPs= ['proxyip.us.fxxk.dedyn.io'];
+const proxyIPs= ['bpb.yousef.isegaro.com'];
 
 const defaultHttpPorts = ['80', '8080', '2052', '2082', '2086', '2095', '8880'];
 const defaultHttpsPorts = ['443', '8443', '2053', '2083', '2087', '2096'];
